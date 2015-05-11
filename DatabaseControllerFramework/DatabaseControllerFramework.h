b2c43dfa92f87c3b315d995b1b2b38b65a85cabb
simioliolio <simioliolio@bitbasic.co.uk>
2015-05-11 12:19:05 +0100

    Initial Commit

---
+++ DatabaseControllerFramework/DatabaseControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  DatabaseControllerFramework.h
+//  DatabaseControllerFramework
+//
+//  Created by Simon Haycock on 11/05/2015.
+//  Copyright (c) 2015 Oxygn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DatabaseControllerFramework.
+FOUNDATION_EXPORT double DatabaseControllerFrameworkVersionNumber;
+
+//! Project version string for DatabaseControllerFramework.
+FOUNDATION_EXPORT const unsigned char DatabaseControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DatabaseControllerFramework/PublicHeader.h>
+
+
